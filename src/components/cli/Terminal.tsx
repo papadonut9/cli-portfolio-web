688bb28d6af996bf9a2b28358964f8a70867dc12
anchxt <d@>
2025-08-30 01:29:19 +0530

    chore: add base Terminal Component and wired it to page.tsx

---
+++ src/components/cli/Terminal.tsx
@@ -0,0 +1,10 @@
+
+export default function Terminal() {
+    return (
+        <div>
+            <p>Welcome to anchxt.dev — CLI Portfolio</p>
+            <p>Type `help` to get started.</p>
+        </div>
+    );
+}
+
