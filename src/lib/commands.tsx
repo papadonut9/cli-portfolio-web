d24996e516165eed766cbcb2eb850a2802e2f840
anchxt <d@>
2025-08-30 03:38:14 +0530

    feat: add fake filesystem with ls/cat commands

--- src/lib/commands.tsx
+++ src/lib/commands.tsx
@@ -1,17 +1,18 @@
-import {JSX} from "react";
 import About from "@/components/cli/commands/About";
 import Help from "@/components/cli/commands/Help";
 import Contact from "@/components/cli/commands/Contact";
 import Projects from "@/components/cli/commands/Projects";
 import Resume from "@/components/cli/commands/Resume";
+import {fakeFS} from "@/lib/utils";
 
-export type CommandHandler = () => JSX.Element | string;
 
-export const commands: Record<string, CommandHandler> = {
+export const commands = {
     help: () => <Help/>,
     about: () => <About/>,
     projects: () => <Projects/>,
     resume: () => <Resume/>,
     contact: () => <Contact/>,
     clear: () => "",
+    ls: () => Object.keys(fakeFS).join("  "),
+    cat: (filename?: string) => fakeFS[filename || ""] || "file not found",
 };
---
+++ src/lib/utils.ts
@@ -0,0 +1,5 @@
+
+export const fakeFS: Record<string, string> = {
+    "README.txt": "Welcome to my portfolio!!",
+    "secrets.txt": "Shh... this is a secret until you find the Konami Code!",
+};
