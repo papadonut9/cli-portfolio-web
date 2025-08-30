7d30e43e0d54f9c013eb8b0294e47e4abbb8321d
anchxt <d@>
2025-08-31 03:45:28 +0530

    feat: enhance Help command with detailed command descriptions
    - Add a comprehensive list of commands with HTML formatting
    - Utilize `dangerouslySetInnerHTML` for rendering rich content

--- src/components/cli/commands/Help.tsx
+++ src/components/cli/commands/Help.tsx
@@ -1,16 +1,23 @@
+import { c } from "@/lib/utils";
 
 export default function Help() {
+    const list = [
+      `<b>help</b>                show help`,
+      `<b>about</b>               who I am`,
+      `<b>projects</b>            list highlighted projects`,
+      `<b>project &lt;id&gt;</b>      show project details`,
+      `<b>resume</b>              quick CV + PDF link`,
+      `<b>contact</b>             email + links`,
+      `<b>shortcuts</b>           handy aliases`,
+      `<b>open &lt;alias|url&gt;</b>  open link in new tab`,
+      `<b>ls</b> / <b>cat &lt;file&gt;</b>   browse tiny VFS`,
+      `<b>theme &lt;dark|light&gt;</b> switch theme`,
+      `<b>clear</b>               clear screen`,
+      `<span class='${c.dim}'>Konami → unlocks: secrets, matrix</span>`,
+    ];
     return (
-        <div>
-            <p>Available commands:</p>
-            <ul>
-                <li>about</li>
-                <li>projects</li>
-                <li>resume</li>
-                <li>contact</li>
-                <li>help</li>
-                <li>clear</li>
-            </ul>
+        <div className="whitespace-pre-wrap">
+            {list.map((s, i) => <span key={i} dangerouslySetInnerHTML={{__html: s}} />)}
         </div>
     );
 }
